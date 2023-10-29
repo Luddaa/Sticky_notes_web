dd5c668e71c51eccbf09de27968d8b2ddc0cf261
Luddaa <dangeloeugene33@gmail.com>
2023-10-29 04:06:19 -0700

    added files with css notes updated

--- frontend/src/components/Note.tsx
+++ frontend/src/components/Note.tsx
@@ -4,21 +4,28 @@ import styles from "../styles/Note.module.css";
 import { Card } from "react-bootstrap";
 import { Note as NoteModel } from "../models/note";
 
+
+
 interface NoteProps {
     note: NoteModel,
 }
 
-const Note = ({ note }: NoteProps) => {
+interface NoteProps {
+    note: NoteModel,
+    className?: string,
+}
+
+const Note = ({ note, className }: NoteProps) => {
     const {
         title,
         text,
-        
-        
     } = note;
 
+
+
     return (
-        <Card className={styles.noteCard}>
-            <Card.Body>
+        <Card className={`${styles.noteCard} ${className}`}>
+            <Card.Body className={styles.cardBody}>
                 <Card.Title>
                     {title}
                 </Card.Title>
